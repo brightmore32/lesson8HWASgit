ec07c13091e64e1a4452ba2d1f33d43b9d2d3b2d
brightmore32 <caleb_brightmore@mail.ru>
2021-09-01 20:29:38 +0300

    make auth layout

--- app/src/main/java/com/brightmore/lesson8hwasgit/MainActivity.java
+++ app/src/main/java/com/brightmore/lesson8hwasgit/MainActivity.java
@@ -3,12 +3,20 @@ package com.brightmore.lesson8hwasgit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.advanced_ui_elements);
+        //setContentView(R.layout.activity_main);
+        //TextView tvTitle = findViewById(R.id.tvTitle);
+        //tvTitle.setText("ПРограммно установленная строка");
+
     }
 }
